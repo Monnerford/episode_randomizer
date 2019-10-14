01ca41dd4e919c461e32465d38a6f276a1eda575
Monnerford <nicomonner@gmail.com>
2019-10-14 02:32:35 -0300

    debugging of paths and possible formats.

--- episode_randomizer.py
+++ episode_randomizer.py
@@ -26,15 +26,19 @@ def episode_randomizer(path='.', save_df=True, open_ep=False):
                 ignore_index=True)
     # Populate DataFrame
     df = df[df['season'] != '.']  # borro este archivo del df
-    df.season = df.season.str.extract(r'(\d+)').astype(int)
-    df.episode_number = df.episode_number.str.extract(r'x(\d+)').astype(int)
-    df.episode_name = df.episode_name.str.extract(r'x\d+\s?_?(.+)')
-    df.episode_name = df.episode_name.str.strip().str.replace(r'\..+', '')
-    df = df.sort_values(['season', 'episode_number'])
-    df.index = df.season.astype(str) + 'X' + df.episode_number.astype(str)
+    try:
+        df.season = df.season.str.extract(r'(\d+)').astype(int)
+        df.episode_number = df.episode_number.str.extract(
+            r'\d+?x?(\d{1, 2})').astype(int)
+        df.episode_name = df.episode_name.str.extract(r'x\d+\s?_?(.+)')
+        df.episode_name = df.episode_name.str.strip().str.replace(r'\..+', '')
+        df = df.sort_values(['season', 'episode_number'])
+        df.index = df.season.astype(str) + 'X' + df.episode_number.astype(str)
+    except Exception as e:
+        print(e, '\nFormat unsupporting df structure, check errors')
     # Write DF to folder
     if save_df:
         df.to_csv(f'{path}/episodes.csv', sep='|')
     if open_ep:
-        open_file(df.sample(1).rel_path[0])
+        open_file(df.sample(1).rel_path.values[0])
     return df.sample(1)
